9613dea18ecd65f280ef22710f4fb3b17a7106d9
DarioGodoy <godoyvalentin@hotmail.com>
2018-07-06 16:37:32 -0300

    Implementacion de tests inicial agent

--- index.js
+++ index.js
@@ -3,8 +3,22 @@
 const setupDatabase = require('./lib/db')
 const setupAgentModel = require('./models/agent')
 const setupMetricModel = require('./models/metric')
+const defaults = require('defaults')
 
 module.exports = async function (config) {
+  config = defaults(config, {
+    dialect: 'mysql',
+    operatorsAliases: false,
+    pool: {
+      max: 5,
+      min: 0,
+      idel: 10000
+    },
+    query: {
+      row: true
+    }
+  })
+
   const sequelize = setupDatabase(config)
   const AgentModel = setupAgentModel(config)
   const MetricModel = setupMetricModel(config)
--- setup.js
+++ setup.js
@@ -1,9 +1,27 @@
 'use strict'
 
-const debug = require('debug')('platziverse:db:setup')
 const db = require('./')
+const debug = require('debug')('platziverse:db:setup')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+
+const prompt = inquirer.createPromptModule()
 
 async function setup () {
+  const answer = await prompt([{
+    type: 'confirm',
+    name: 'setup',
+    message: 'This will destroy tour database, are you sure?'
+  }])
+
+  if (!answer.setup) {
+    return console.log(chalk.cyan(`
+    ------------------
+     Nothing happened
+    ------------------
+    `))
+  }
+
   const config = {
     database: process.env.DB_NAME || 'platziverse',
     username: process.env.DB_USER || 'root',
@@ -16,13 +34,16 @@ async function setup () {
   }
 
   await db(config).catch(handleFatalError)
-
-  console.log('success!')
+  console.log(chalk.green(`
+  ------------------
+  ---- success! ----
+  ------------------
+  `))
   process.exit(0)
 }
 
 function handleFatalError (err) {
-  console.error(err.message)
+  console.error(`${chalk.red('[ERROR FATAL]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
 }
